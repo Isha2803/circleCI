280abe9aa975386683fdff56ecbbc5864d8028f9
Isha <68682025+Isha2803@users.noreply.github.com>
2024-10-04 16:32:38 +1300

    Update the default name

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ def to_upper(name):
 def say_hello(name):
     print(f'Hello, {name}')
     if __name__ == '__main__':
-        name = 'Jack'
+        name = 'Isha'
         say_hello(name)
         up = to_upper(name)
         print(up)
